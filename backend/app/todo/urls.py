b6ad4964596d7e6c0cc10fe06c99295c5c626a47
Влад Шарапа <vl.sharapa20@gmail.com>
2023-08-24 13:26:55 +0300

    Setting endpoints of api

---
+++ backend/app/todo/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('task/list/', task_list, name='task_list'),
+    path('task/create/', TaskCreate.as_view()),
+    path('task/update/<int:pk>', TaskUpdate.as_view())
+]
