cccc886143488cc23d3403dd1ebcb149a4dcce17
Влад Шарапа <vl.sharapa20@gmail.com>
2023-08-24 13:27:26 +0300

    Create common views

--- backend/app/todo/views.py
+++ backend/app/todo/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    
+    if serializer.data:
+        return Response(serializer.data)
+    
+    return Response({'message': 'No data in database'})
+
+
+class TaskCreate(APIView):
+    
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'invalid data input'})
+        
+
+class TaskUpdate(APIView):
+
+    def post(self, request, pk):
+        try:
+             instance = Task.objects.get(id=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+       
+        serializer = TaskSerializer(instance=instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"task": serializer.data})
+
