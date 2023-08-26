1693692d467a9c4e09e1c0fad64592768cb6b15b
Влад Шарапа <vl.sharapa20@gmail.com>
2023-08-26 17:10:49 +0300

    Add form component for creating new tasks

---
+++ frontend/src/components/screens/home/task-form/TaskForm.jsx
@@ -0,0 +1,56 @@
+import './TaskForm.css'
+import { useEffect, useState } from 'react'
+import { TaskService } from '../../../../services/task.service'
+
+const Task = {
+    id: null,
+    title: '',
+    description: '',
+    completed: false
+}
+
+
+export default function TaskForm({setTasks}){
+
+    const [data, setData] = useState(Task)
+
+    function handleAddTask(e){
+        e.preventDefault()
+        console.log(data)
+        handleSubmit()
+        setData(Task)
+    }
+
+    function handleSubmit(){
+        const fetchTasks = async() => {
+            const response = await TaskService.submitTask(data)
+            
+                console.log('Go')
+                setTasks(prev => [response, ...prev])
+           
+        }
+        fetchTasks()
+    }
+
+    return(
+        <div id="form-wrapper">
+            <form onSubmit={handleSubmit}>
+                <div className="form-wrapper">
+                    <input className="form-control"
+                           id='title' 
+                           type="text"  
+                           placeholder="Title"
+                           onChange={e => setData(prev => ({...prev, title:e.target.value}))}
+                           value={data.title}/>
+                    <input className="form-control" 
+                           id='description' 
+                           type="text"  
+                           placeholder="Description"
+                           onChange={e => setData(prev => ({...prev, description:e.target.value}))}
+                           value={data.description}/>
+                    <input onClick={e => handleAddTask(e)} className="btn btn-warning" id='btn-submit' type="submit" name='Add' />
+                </div>
+            </form>
+        </div>
+    )
+ }
