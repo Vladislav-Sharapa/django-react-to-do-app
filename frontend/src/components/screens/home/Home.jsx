74f868a90361798516075312be6e771a58113d6a
Влад Шарапа <vl.sharapa20@gmail.com>
2023-08-26 17:12:07 +0300

    Create home component

--- frontend/src/components/screens/home/Home.jsx
+++ frontend/src/components/screens/home/Home.jsx
@@ -1,3 +1,30 @@
+import React, { useState, useEffect } from "react"
+import { TaskService } from "../../../services/task.service"
+import TaskForm from "./task-form/TaskForm"
+import TaskList from "./task-list/TaskList"
+import './Home.css'
+
 export default function Home(){
 
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() =>{
+        const fetchData = async() => {
+            console.log('Fetching...')
+            const responce = await TaskService.getAll()
+            
+            setTasks(responce)
+        }
+
+        fetchData()
+    }, [])
+
+    return(
+        <div className="container">
+            <div id="task-container">
+                <TaskForm setTasks={setTasks}/>
+                <TaskList tasks={tasks}/>
+            </div>
+        </div>
+    )
 }
