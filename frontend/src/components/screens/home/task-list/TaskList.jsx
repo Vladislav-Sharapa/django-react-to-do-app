ab4c98f2c18e0bea5e466936437ed66a480704a9
Влад Шарапа <vl.sharapa20@gmail.com>
2023-08-29 11:05:53 +0300

    Create component for output tasks

---
+++ frontend/src/components/screens/home/task-list/TaskList.jsx
@@ -0,0 +1,57 @@
+import { TaskService } from '../../../../services/task.service'
+import './TaskList.css'
+
+export default function TaskList({tasks = [], setTasks}){
+
+    return(
+        <div className="list-wrapper">
+            <div className="flex-wrapper header">
+                <div style={{flex:2}}>
+                    <span>Title</span>
+                </div>
+                             
+                <div style={{flex:5}}>
+                    <span>Description</span>
+                 </div>
+            </div>
+            {tasks.map(function(task, index){
+                return(
+                        <div onClick={() => {
+                            handleUpdate(task.id, {
+                                title: task.title,
+                                completed: !task.completed
+                            })
+                        }} key={index} className="task-wrapper flex-wrapper">
+                            {task.completed == false ?(
+                                <>
+                                    <div style={{flex:2}}>
+                                        <span>{task.title}</span>
+                                    </div>
+
+                                    <div style={{flex:3.8}}>
+                                        <span>{task.description}</span>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                <div style={{flex:2}}>
+                                    <strike>{task.title}</strike>
+                                </div>
+
+                                <div style={{flex:3.8}}>
+                                    <strike>{task.description}</strike>
+                                </div>
+                            </>  
+                            )}
+                            <div style={{flex:0}}>
+                                <button className="btn  btn-outline-secondary ">Edit</button>
+                            </div>
+                            <div style={{flex:0}}>
+                                <button className="btn btn-outline-danger">Del</button>
+                            </div>
+                        </div>
+                    )
+                })}
+        </div>
+    )
+}
